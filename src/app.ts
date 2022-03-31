68ef97da4c8c9415a290ab0b1e0c8d54c85b39f6
dmitriyyan <dmitriy.ya@protonmail.com>
2022-03-31 18:30:56 +0300

    add input elements to class

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 class ProjectInput {
 	templateEl: HTMLTemplateElement;
 	appEl: HTMLDivElement;
+	formEl: HTMLFormElement;
+	titleInputEl: HTMLInputElement;
+	descriptionInputEl: HTMLTextAreaElement;
+	peopleInputEl: HTMLInputElement;
 
 	constructor() {
 		this.templateEl = document.getElementById(
@@ -10,7 +14,32 @@ class ProjectInput {
 		this.appEl = document.getElementById('app')! as HTMLDivElement;
 
 		const importedNode = document.importNode(this.templateEl.content, true);
-		this.appEl.appendChild(importedNode);
+		this.formEl = importedNode.firstElementChild as HTMLFormElement;
+
+		this.titleInputEl = this.formEl.querySelector(
+			'#title'
+		)! as HTMLInputElement;
+		this.descriptionInputEl = this.formEl.querySelector(
+			'#description'
+		)! as HTMLTextAreaElement;
+		this.peopleInputEl = this.formEl.querySelector(
+			'#people'
+		)! as HTMLInputElement;
+
+        this.configureForm();
+		this.renderForm();
+	}
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+    }
+
+	private configureForm() {
+		this.formEl.addEventListener('submit', this.submitHandler.bind(this));
+	}
+
+	private renderForm() {
+		this.appEl.appendChild(this.formEl);
 	}
 }
 
