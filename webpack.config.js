c0c95b3a8a22bf1a6544e7d0c26dfed34c1f869b
dmitriyyan <dmitriy.ya@protonmail.com>
2022-04-01 17:03:22 +0300

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+	entry: './src/app.ts',
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: 'dist',
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		static: {
+			directory: path.join(__dirname, '.'),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+};
